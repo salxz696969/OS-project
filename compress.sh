44afcc07bbd7321b26b27665b384354b035daf5f
Chanraksa Ory <OCraksa@gmail.com>
2025-04-02 15:46:27 +0700

    changed compress abit

--- compress.sh
+++ compress.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-
-# === Function to Compress Files or Directories ===
 compress_files() {
-    # *** Prompt for Source and Destination ***
     read -r -p "Enter the file or directory to compress: " source
     read -r -p "Enter the name for the compressed file (without extension): " destination
-
-    # *** Check if Source Exists ***
     if [ -e "$source" ]; then
-        # Compress the source into a .tar.gz file
-        tar -czvf "$destination.tar.gz" "$source"
-        log_action "Compressed $source to $destination.tar.gz"
+        zip -r "$destination.zip" "$source"
+        log_action "Compressed $source to $destination.zip"
     else
         echo "Source does not exist."
     fi
 }
-
-# === End of Script ===
