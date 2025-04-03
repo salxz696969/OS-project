cbbd2461eb52a392f6254a8316da3ec2434007d9
Chanraksa Ory <OCraksa@gmail.com>
2025-04-03 10:27:45 +0700

    changed var name in compress

--- compress.sh
+++ compress.sh
@@ -4,11 +4,11 @@ compress_files() {
     echo -e "\n==== Compress Files Utility ====\n"
     
     read -r -p "Enter the file or directory to compress: " source
-    read -r -p "Enter the name for the compressed file (without extension): " destination
+    read -r -p "Enter the name for the compressed file (without extension): " name
     
     if [ -e "$source" ]; then
-        zip -r "$destination.zip" "$source"
-        log_action "Compressed $source to $destination.zip"
+        zip -r "$name.zip" "$source"
+        log_action "Compressed $source to $name.zip"
         echo -e "\n**** Compression Successful! ****\n"
     else
         echo -e "\n**** Error: Source does not exist. ****\n"
